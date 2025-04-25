5da0ef99aa960f4f61eee61a570f31b5568e957d
Learner777-ai <l8414679@gmail.com>
2025-04-25 23:36:31 +0530

    requiremetns add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="mann",
+    author_email="l8414679@gmail.com",
+    packages=find_packages()
+)
